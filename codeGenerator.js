ecc68fdf7c80fbc39644e86fadf12e6609ecd1be
RDziin <rhodrigo.leandro@gmail.com>
2024-10-31 17:00:51 -0300

    Primeiro Commit

---
+++ codeGenerator.js
@@ -0,0 +1,9 @@
+export function codeGenerator(tamanho){
+    let caracter = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    let codigo = ""
+
+    for(let i = 0; i <tamanho; i++)
+        codigo += caracter.charAt(Math.floor(Math.random() * caracter.length))
+
+    return codigo;
+}
---
+++ main.js
@@ -0,0 +1,33 @@
+import { userCreate } from "./Users.js";
+import { userList } from "./Users.js";
+import { userDelete } from "./Users.js";
+
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
+const readline = require('readline-sync');
+
+let rep = true
+
+do{
+    console.clear()
+    let opt = readline.questionInt('1 - Criar \n2 - Listar \n3 - Deletar \n0 - Sair \nEscolha: ')
+
+    switch(opt){
+        case 1:
+            userCreate()
+            break
+        case 2:
+            userList()
+            break
+        case 3:
+            userDelete()
+            break
+        case 0:
+            console.log('Saindo...')
+            rep = false
+            break
+        default:
+            console.log('Opcao Não existente')
+            break
+    }
+} while(rep)
